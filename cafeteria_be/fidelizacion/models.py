51c79adea5ca26114522d41f525a5269aaeb6fb1
Samuel Fernandez <samueve24@gmail.com>
2026-06-20 17:26:00 -0300

    bolsas y usos

--- cafeteria_be/fidelizacion/models.py
+++ cafeteria_be/fidelizacion/models.py
@@ -58,3 +58,47 @@ class VencimientoPuntos(models.Model):
         return f'{self.fecha_inicio} - {self.fecha_fin}'
 
 
+class BolsaPuntos(models.Model):
+    id = models.IntegerField(primary_key=True)
+    cliente_id = models.IntegerField()
+    fecha_asignacion = models.DateField()
+    fecha_caducidad = models.DateField()
+    puntaje_asignado = models.IntegerField(default=0)
+    puntaje_utilizado = models.IntegerField(default=0)
+    saldo_puntos = models.IntegerField(default=0)
+    monto_operacion = models.IntegerField(default=0)
+    vencido = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['fecha_asignacion', 'id']
+
+    def __str__(self):
+        return f'Bolsa {self.id} - Cliente {self.cliente_id}'
+
+
+class UsoPuntos(models.Model):
+    id = models.IntegerField(primary_key=True)
+    cliente_id = models.IntegerField()
+    concepto_id = models.IntegerField()
+    puntaje_utilizado = models.IntegerField(default=0)
+    fecha = models.DateField()
+    detalle = models.JSONField(default=list)
+
+    class Meta:
+        ordering = ['-fecha', '-id']
+
+    def __str__(self):
+        return f'Uso {self.id} - Cliente {self.cliente_id}'
+
+
+class ProcesoFidelizacion(models.Model):
+    id = models.IntegerField(primary_key=True)
+    ultima_ejecucion = models.DateTimeField(null=True, blank=True)
+    proxima_ejecucion = models.DateTimeField(null=True, blank=True)
+    bolsas_vencidas = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return 'Proceso de fidelizacion'
