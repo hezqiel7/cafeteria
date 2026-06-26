f79f11c6cd58b41abd2f59a80a524728561b7969
Samuel Fernandez <samueve24@gmail.com>
2026-06-26 15:43:00 -0300

    permisos y conf

--- cafeteria_be/cafeteria_be/permissions.py
+++ cafeteria_be/cafeteria_be/permissions.py
@@ -1,9 +1,23 @@
+from django.contrib.auth.models import Group, User
 from rest_framework import permissions
-    
+
+
+def user_has_group(user, group_name):
+    if not user or not user.is_authenticated:
+        return False
+
+    group = Group.objects.filter(name=group_name).first()
+    if not group:
+        return False
+
+    return User.groups.through.objects.filter(user_id=user.id, group_id=group.id).first() is not None
+
+
 class IsRecepcionista(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.get().name == 'recepcion'
-    
+        return user_has_group(request.user, 'recepcion')
+
+
 class IsCocinero(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.get().name == 'cocina'
+        return user_has_group(request.user, 'cocina')
--- cafeteria_be/cafeteria_be/settings.py
+++ cafeteria_be/cafeteria_be/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     "productos",
     "pedidos",
     "usuarios",
+    "fidelizacion",
     "corsheaders",
 ]
 
